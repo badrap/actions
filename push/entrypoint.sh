daa7f3265fb672a800a245b7913a6031f3b86965
Joachim Viide <jviide@iki.fi>
2019-11-18 17:55:44 +0200

    Update

--- push/entrypoint.sh
+++ push/entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/sh -l
 
-echo ${GITHUB_REPOSITORY}
+echo "${INPUT_GCLOUD_SERVICE_KEY}" > ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account --project=${INPUT_GCLOUD_PROJECT} --key-file=${HOME}/gcloud-service-key.json
+gcloud auth configure-docker
+
+OUTPUT_IMAGE="${INPUT_GCLOUD_CONTAINER_REPOSITORY}:${GITHUB_SHA}"
+docker tag "${INPUT_INPUT_IMAGE}" "${OUTPUT_IMAGE}"
+docker push "${OUTPUT_IMAGE}"
+
+echo ::set-output name=output_image::"${OUTPUT_IMAGE}"
