d110f8b007fe913860899232e3ebe79e5f0f2bd2
Joachim Viide <jviide@iki.fi>
2019-11-18 23:23:45 +0200

    Fix deploy/entrypoint.sh

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -7,5 +7,5 @@ echo "${INPUT_GCLOUD_SERVICE_KEY}" > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --project "${INPUT_GCLOUD_PROJECT}" --key-file=${HOME}/gcloud-service-key.json
 gcloud --quiet container clusters get-credentials -z "${INPUT_CLUSTER_ZONE}" "${INPUT_CLUSTER_NAME}"
 
-GOOGLE_APPLICATION_CREDENTIALS="${HOME}/gcloud-service-key.json"
+export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/gcloud-service-key.json"
 sops -d "${INPUT_SECRETS_PATH}" | helm upgrade "${INPUT_HELM_RELEASE}" helm-chart --install --atomic --namespace "${INPUT_HELM_NAMESPACE}" --values - --set image.repository="${GCLOUD_CONTAINER_REGISTRY}" --set image.tag="${GITHUB_SHA}"
