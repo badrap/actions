468f535079bf93f93d1ab8f1046bbfe463692209
Joachim Viide <jviide@iki.fi>
2020-06-04 18:07:04 +0300

    Create deployment namespace if it doesn't exist yet

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -8,4 +8,4 @@ gcloud auth activate-service-account --project "${INPUT_GCLOUD_PROJECT}" --key-f
 gcloud --quiet container clusters get-credentials -z "${INPUT_CLUSTER_ZONE}" "${INPUT_CLUSTER_NAME}"
 
 export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/gcloud-service-key.json"
-sops -d "${INPUT_SECRETS_PATH}" | helm upgrade "${INPUT_HELM_RELEASE}" helm-chart --install --atomic --namespace "${INPUT_HELM_NAMESPACE}" --values - --set image.repository="${INPUT_GCLOUD_CONTAINER_REPOSITORY}" --set image.tag="${GITHUB_SHA}"
+sops -d "${INPUT_SECRETS_PATH}" | helm upgrade "${INPUT_HELM_RELEASE}" helm-chart --install --atomic --namespace "${INPUT_HELM_NAMESPACE}" --create-namespace --values - --set image.repository="${INPUT_GCLOUD_CONTAINER_REPOSITORY}" --set image.tag="${GITHUB_SHA}"
